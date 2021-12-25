99e954b87e7e243a67ba38622de44c44f987f3a2
erikliu123 <1171045702@qq.com>
2021-12-25 13:30:13 +0000

    add cuda lib

--- lib/nvmf/ndp.h
+++ lib/nvmf/ndp.h
@@ -2,9 +2,10 @@
 #ifndef __NDP_H__
 #define __NDP_H__
 
-//#include "spdk/bdev.h"
-//#include "spdk/nvmf_transport.h"
-
+#define DEFAULT_NDP_DIR "/mnt/ndp/"
+#define MB (1024 * 1024)
+#define MAX_GRAPH (1024 * 1024)
+#define MAX_COMPRESS_SIZE (MAX_GRAPH) //分大一点似乎好一点？
 struct ndp_request
 {
     struct spdk_nvmf_request *nvmf_req;
---
+++ lib/nvmf/ndp_cuda.h
@@ -0,0 +1,102 @@
+#ifndef DDS_H
+#define DDS_H
+
+
+typedef union
+{
+  struct
+  {
+    unsigned char r,g,b,a;//b, g, r, a; /*if stay same, decompress image will have problem*/
+  };
+  unsigned int u;
+} Color32;
+
+typedef union
+{
+  struct
+  {
+    unsigned short b : 5;
+    unsigned short g : 6;
+    unsigned short r : 5;
+  };
+  unsigned short u;
+} Color16;
+// int func(void);
+typedef struct
+{
+  Color16 col0;
+  Color16 col1;
+  union
+  {
+    unsigned char row[4];
+    unsigned int indices;
+  };
+  // void decompress(Color32 colors[16]) const;
+} BlockDXT1;
+
+struct cudaPreAlloc
+{
+  struct {
+    Color32 *decompressResult;
+    BlockDXT1 *inputImage;
+  } compressTask;
+
+};
+
+/*
+    图像压缩/解压缩相关
+*/
+#if !defined(MAKEFOURCC)
+#define MAKEFOURCC(ch0, ch1, ch2, ch3)                \
+  ((unsigned int)(ch0) | ((unsigned int)(ch1) << 8) | \
+   ((unsigned int)(ch2) << 16) | ((unsigned int)(ch3) << 24))
+#endif
+
+typedef unsigned int uint;
+typedef unsigned short ushort;
+
+struct DDSPixelFormat {
+  uint size;
+  uint flags;
+  uint fourcc;
+  uint bitcount;
+  uint rmask;
+  uint gmask;
+  uint bmask;
+  uint amask;
+};
+
+struct DDSCaps {
+  uint caps1;
+  uint caps2;
+  uint caps3;
+  uint caps4;
+};
+
+/// DDS file header.
+struct DDSHeader {
+  uint fourcc;
+  uint size;
+  uint flags;
+  uint height;
+  uint width;
+  uint pitch;
+  uint depth;
+  uint mipmapcount;
+  uint reserved[11];
+  DDSPixelFormat pf;
+  DDSCaps caps;
+  uint notused;
+};
+
+static const uint FOURCC_DDS = MAKEFOURCC('D', 'D', 'S', ' ');
+static const uint FOURCC_DXT1 = MAKEFOURCC('D', 'X', 'T', '1');
+static const uint DDSD_WIDTH = 0x00000004U;
+static const uint DDSD_HEIGHT = 0x00000002U;
+static const uint DDSD_CAPS = 0x00000001U;
+static const uint DDSD_PIXELFORMAT = 0x00001000U;
+static const uint DDSCAPS_TEXTURE = 0x00001000U;
+static const uint DDPF_FOURCC = 0x00000004U;
+static const uint DDSD_LINEARSIZE = 0x00080000U;
+
+#endif  // DDS_H
